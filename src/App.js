308a2b915c8d96a75ad54808c53983c27f8e318a
Gourav Roy <gouravroy7209@gmail.com>
2020-09-09 20:42:18 +0530

    configuration-status

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import BlogCard from './BlogCard';
-class App extends React.Component {
+class App extends Component {
   render(){
    return (
     <div className="App">
-       <Navbar darkcolor="primary">
+       <Navbar dark color="primary">
          <div className="container">
            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
          </div>
