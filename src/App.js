313c1637febaee0f52acdf685af8e0cdeb6a8740
Gourav Roy <gouravroy7209@gmail.com>
2020-09-12 19:18:08 +0530

    header-footer-updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import Main from './Components/MainComponent';
+import './App.css';
 class App extends Component {
 
   render(){
+    // git push -u origin master
    return (
     <div className="App">
        <Main />  
--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+
 import Menu from './MenuComponent';
 import {DISHES} from '../shared/dishes';
 import DishDetails from './DishDetails';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 
 class Main extends Component {
 
@@ -24,13 +26,10 @@ class Main extends Component {
 
    return (
     <div className="App">
-       <Navbar dark color="primary">
-         <div className="container">
-           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-         </div>
-       </Navbar>
+        <Header />
         <Menu dishes={this.state.dishes} onClick = {(dishId) => this.onDishSelect(dishId)}/>
         <DishDetails dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />  
+        <Footer />
      </div>
     );
   }
